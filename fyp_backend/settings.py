5ab61970b121b13bfad00a37a15b99850d0a5eba
YounesDjelloul <younesdjelloul14@gmail.com>
2025-01-07 19:27:48 +0800

    using dj database to gracefully parse database url

--- fyp_backend/settings.py
+++ fyp_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,10 +109,10 @@ WSGI_APPLICATION = 'fyp_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql' if os.environ.get('DATABASE_URL') else 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('DATABASE_URL') or BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 # Password validation
